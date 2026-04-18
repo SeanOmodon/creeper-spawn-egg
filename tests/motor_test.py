3ef335c26f09ce9ecbc2d4aa610d1760e98bd210
yuizhu0 <yuizhu1217@gmail.com>
2026-04-18 14:53:29 -0500

    updated numbers

--- tests/motor_test.py
+++ tests/motor_test.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 try:
     while True:
-        GPIO.output(17, GPIO.HIGH)
-        GPIO.output(27, GPIO.HIGH)
+        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(12, GPIO.HIGH)
         time.sleep(2)
-        GPIO.output(17, GPIO.LOW)
-        GPIO.output(27, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
         time.sleep(2)
 except KeyboardInterrupt:
     GPIO.cleanup()
