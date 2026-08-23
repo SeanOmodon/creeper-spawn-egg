de5ca4d59b3e3329a44b66c5a81282c7298ca191
Sean Omodon <seanomodon3@gmail.com>
2026-08-23 18:18:07 -0500

    fix: reduce sleep duration before playing explosion in main function

--- USB_tests/07_buzzer_test_usb.py
+++ USB_tests/07_buzzer_test_usb.py
@@ -90,7 +90,7 @@ def main():
     print("Buzzer playing creeper hiss.")
     buzzer.play_creeper_hiss()
     buzzer.off()
-    time.sleep(1000)
+    time.sleep(1)
     print("Buzzer playing explosion.")
     buzzer.play_explosion()
     buzzer.off
